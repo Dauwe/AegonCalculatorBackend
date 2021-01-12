60d3f8f33fe01e2479f8e98ebb3fdbf80ca6b0cf
dschimmel <dschimmel@quintor.nl>
2021-01-12 15:50:37 +0100

    Crossorigin for FE

--- src/main/java/nl/aegon/calculator/web/CalculatorController.java
+++ src/main/java/nl/aegon/calculator/web/CalculatorController.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/calculator")
+@CrossOrigin
 public class CalculatorController {
     final SimpleCalculator simpleCalculator;
 
